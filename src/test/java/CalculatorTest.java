05ce60d9c59a7273db7ce0c4328c64e43c99c8dc
Rodrigo Souza <rodrigo_rios@Rodrigos-MacBook-Pro.local>
2021-07-20 13:04:17 -0300

    commit Rodrigo, java UnitCalculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,11 +1,31 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+  @BeforeEach
+  private JUnitCalculator jUnitCalculator = new JUnitCalculator():
+
+
   @Test
   public void testSanityCheck() {
     assertEquals("Hello", "Hello");
   }
+
+  @Test
+  public void testSum() {
+    assertEquals( 4 , JUnitCalculator.add(2,2) );
+
+  }
 }
+
+
+
+
+
+
+
