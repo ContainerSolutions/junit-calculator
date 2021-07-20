a299f5dd6c95dde8ceab5060ce3819cace565549
Tibor Eszes <tibor.eszes@container-solutions.com>
2021-07-20 17:38:50 +0200

    Tibor

---
+++ src/main/java/JUnitCalculator.java
@@ -0,0 +1,5 @@
+public class JUnitCalculator {
+  public void main() {
+    System.out.println("Hello World");
+      }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+  @Test
+  public void testSanityCheck() {
+    assertEquals("Hello", "Hello");
+  }
+}
